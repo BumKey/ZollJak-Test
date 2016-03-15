4718def15351254ad30c0b7db1c74865e8ed90ec
BumKey <pk_tiger@naver.com>
2016-03-15 22:17:27 +0900

    Revert "메인 파일 수정과 클래스추가"
    This reverts commit 35a175fc585339cdd8431559537332ae75ad872e.

--- test_bumkey/test_bumkey/main.cpp
+++ test_bumkey/test_bumkey/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "kbPark.h"
-#include "yongjun.h"
+
 using namespace std;
 
 int main()
 {
 	kbPark a;
 	a.Print_BumKey_Test();
-	yongjun b;
-	b.Print_YoungJun_Test();
-
 }

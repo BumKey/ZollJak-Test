00b07ffd856af359d79191a5a45b4ca9acaece2e
jun ddd <pcqkdrh222@gmail.com>
2016-05-14 02:21:02 +0900

    로직 - 웨이브, 공격, 간단한 길찾기, 게임진행

---
+++ Skatus Defense/Scatus Defence/Scatus Defence/GameFramework.h
@@ -0,0 +1,38 @@
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Effects.h"
+#include "Vertex.h"
+#include "Camera.h"
+#include "RenderStates.h"
+#include "TextureMgr.h"
+#include "BasicModel.h"
+#include "SceneMgr.h"
+
+#include "GameRogicManager.h"
+class GameFrameWork : public D3DApp
+{
+public:
+	GameFrameWork(HINSTANCE hInstance);
+	~GameFrameWork();
+	
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+
+	TextureMgr mTexMgr;
+	SceneMgr mSceneMgr;
+	GameRogicManager *mGameRogicMgr;
+	POINT mLastMousePos;
+
+};

0190eaf4e2d721ba3ccf7111c26a00a95721a7b0
giljune91 <adda281@naver.com>
2016-06-03 22:13:54 +0900

    dddddd

---
+++ GameServer/GameServer/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <WinSock2.h>
+#include <Windows.h>
+
+#include <vector>
+#include <thread>
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+#include <mutex>
+#include <set>
+
+#include "protocol.h"
